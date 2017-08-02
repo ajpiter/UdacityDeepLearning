bdd31cfe3db448288b336d4826ea099c9ecf55f2
AJ Piter <amanda.piter@gmail.com>
2017-08-02 09:58:53 -0400

    Create InstallingAnaconda.py

---
+++ InstallingAnaconda.py
@@ -0,0 +1,30 @@
+#Start Here: https://www.continuum.io/downloads
+
+#On the terminal: 
+conda upgrade conda 
+conda upgrade --all 
+
+#to install packages 
+conda install numpy 
+
+#to install multiple packages 
+conda install numpy pandas 
+
+#to install specific verisons 
+conda install numpy 1.10 
+
+#to unistall 
+conda remove numpy 
+
+#to update 
+conda update numpy 
+#or 
+conda update --all
+
+#to list installed packages 
+conda list 
+
+#If you don't know the exact package you're looking for you can search 
+conda search search_term 
+#helpful if you don't know the package name 
+conda search beautifulsoup 
