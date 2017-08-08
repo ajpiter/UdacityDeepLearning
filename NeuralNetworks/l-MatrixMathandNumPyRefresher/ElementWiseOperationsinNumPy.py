37a8ca3a7254de6087e8814545376cdf731727b3
AJ Piter <amanda.piter@gmail.com>
2017-08-08 09:32:19 -0400

    Create ElementWiseOperationsinNumPy.py

---
+++ NeuralNetworks/l-MatrixMathandNumPyRefresher/ElementWiseOperationsinNumPy.py
@@ -0,0 +1,19 @@
+#To add 5 to every item in a list using NumPy by creating an array 
+listvariable = [1,2,3,4,5] 
+listvariable = np.array(listvariable) + 5 
+#returns [6,7,8,9,10] 
+
+#If you already had an array 
+arrayvariable += 5 
+
+#NumPy has functions to do math, or you can use math operations 
+variable = np.multipy(arrayvariable, 5) 
+#or 
+variable = arrayvariable * 5 
+
+#To reuse a matrix, set all its values to zero 
+matrix *= 0 
+
+#Squared value of a matrix 
+variable = matrix * matrix 
+
