b6ff35bc6bc6abfbcb562d58ea29cae33f800d34
AJ Piter <amanda.piter@gmail.com>
2017-08-08 09:36:03 -0400

    Update Element-wiseMatrixOperations.py

--- NeuralNetworks/l-MatrixMathandNumPyRefresher/Element-wiseMatrixOperations.py
+++ NeuralNetworks/l-MatrixMathandNumPyRefresher/Element-wiseMatrixOperations.py
@@ -9,11 +9,19 @@
 #Adding two scalars 
 2 + 3 = 5 
 
-#Adding a scalar and a matrix 
-2 + [ 1 2  = [3 4
-      3 4 ]   5 6] 
+#Adding a scalar and a matrix
+#represented on paper by 
+#2 + [ 1 2  = [3 4
+#      3 4 ]   5 6] 
+
       
 #Adding two matices 
 #Have to be the same shape, cannot add a (2, 2) matrix with a (3, 4) matrix 
-[ 1 3   +   [ 2 4   =   [ 3 7
-  5 7]        6 8]        11 15] 
+#represented on paper
+#[ 1 3   +   [ 2 4   =   [ 3 7
+# 5 7]        6 8]        11 15] 
+a = np.array([[1, 3], [5,7]])
+b = np.array([[2, 4], [6,8]]) 
+a + b 
+#returns array([[3, 7], 
+#               [11, 15]])
