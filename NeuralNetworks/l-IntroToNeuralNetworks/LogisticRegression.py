b27ce02dc35d380b53f27357c40ea6aa00c697db
AJ Piter <amanda.piter@gmail.com>
2017-08-08 13:27:01 -0400

    Update LogisticRegression.py

--- NeuralNetworks/l-IntroToNeuralNetworks/LogisticRegression.py
+++ NeuralNetworks/l-IntroToNeuralNetworks/LogisticRegression.py
@@ -1,1 +1,11 @@
-#
+#Logistic Regression 
+#A line that best seperates the data 
+#Computer draws a random line 
+#Then calculates the number of errors 
+
+#Gradient Descent 
+#Finding the best line fit by minizing the error 
+#By moving the line to find smaller sum of errors 
+
+#Neural Network 
+#Has two questions or more questions 
