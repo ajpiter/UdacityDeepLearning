fa1219c1b090d8bc22204c8fac413726f19e5a4c
AJ Piter <amanda.piter@gmail.com>
2017-08-15 15:13:09 -0400

    Update SigmoidActivationFunction.py

--- NeuralNetworks/l-IntroToNeuralNetworks/SigmoidActivationFunction.py
+++ NeuralNetworks/l-IntroToNeuralNetworks/SigmoidActivationFunction.py
@@ -1,3 +1,9 @@
+#The sigmoid Activation Function is also known as the logistic function 
+
+#The sigmoid function is bounded bewteen 0 and 1 
+#The output equals the probability for success 
+#Same formula as logistic regression 
+
 # Defining the sigmoid function for activations
 def sigmoid(x):
     return 1/(1+np.exp(-x))
@@ -5,3 +11,13 @@ def sigmoid(x):
 # Derivative of the sigmoid function
 def sigmoid_prime(x):
     return sigmoid(x) * (1 - sigmoid(x))
+
+import numpy as np
+def sigmoid(x): 
+    return 1/(1 + np.exp(-x))
+inputs = np.array([0.7, -0.3])
+weights = np.array([0.1, 0.8])
+bias = -0.1 
+output = sigmoid(np.dot(weights, inputs) + bias) 
+print(output) 
+
